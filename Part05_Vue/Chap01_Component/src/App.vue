596ba6e704534a6cfc4413406638d1d92be2f7aa
SHINSUNGJIN <pxp13716@gmail.com>
2021-02-04 14:58:45 +0900

    Vue Start

---
+++ Part05_Vue/Chap01_Component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-body">
+    <h1>Vue Components</h1>
+
+    <A01Currency></A01Currency>
+    
+  </div>
+</template>
+
+<style>
+  
+  h1 { color: orange; }
+
+</style>
+
+<script>
+import A01Currency from './components/A01.Currency';
+
+export default {
+  name: 'App',
+  components: {
+    A01Currency, 
+  }
+}
+</script>
+
+
---
+++ Part05_Vue/Chap01_Component/src/components/A01.Currency.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>1. Currency</h3>
+        <br>
+
+        <div>
+        Qty: <input type="text" class="form-control"><br>
+        Cost: <input type="text" class="form-control"><br>
+        Country: 
+            <select class="form-control">
+                <option></option>
+            </select>
+        </div>
+        <br>
+
+        <div>
+            Total: <span></span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
