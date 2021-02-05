a7044987ffdf535d07fc5adeacd39323d4cb77e1
SHINSUNGJIN <pxp13716@gmail.com>
2021-02-05 10:54:45 +0900

    Vue Chap03 Todo Complete

---
+++ Part05_Vue/Chap03_Todo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TodoTemplate></TodoTemplate>
+  </div>
+</template>
+
+<script>
+import TodoTemplate from './components/TodoTemplate';
+export default {
+  name: 'App',
+  components: {
+    TodoTemplate
+  }
+}
+</script>
+
+<style>
+
+</style>
